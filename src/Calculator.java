e2e815e389962bbedb1135ffe40814032c018a7d
irfanchattha <chattha.irfan@gmail.com>
2019-10-28 18:25:58 +0500

    Testing 1st Time
    first commit to master branch

---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Calculator myCalc = new Calculator();
+	//FirefoxDriver driver= new FirefoxDriver();
+	
+		
+	}
+
+	public Calculator() {
+		
+		System.out.println("This is constructor code called");
+	}
+	
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("This is add function called");
+		
+	}
+
+	public void sub(){
+		System.out.println("This is subtract function called");
+	
+	}
+
+	public void div() {
+		System.out.println("This is divide function called");
+	}
+	public void mul() {
+		System.out.println("This is multiply fuction called");
+		
+	}
+
+}
